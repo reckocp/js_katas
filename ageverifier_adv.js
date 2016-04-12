98c568f83db5be8574b0287ff3f3b0c89dc03542
COLIN <reckocp@gmail.com>
2016-04-12 15:50:39 -0500

    Fixing formatting

--- ageverifier_adv.js
+++ ageverifier_adv.js
@@ -1,17 +1,16 @@
-var age = []
-
 var ageVerifier = function (age) {
-  for(var i = 0; i < age.length; i++){
-    if (age[i] < 21) {
+    for(var i = 0; i < age.length; i++){
+      if (age[i] < 21) {
         console.log("You are " + age[i] + ". You are too young.");
-    } else {
+      } else {
         console.log("You are " + age[i] + ". Proceed.");
+      }
     }
-  }
 };
 
 ageVerifier([15,25,30]);
 
 
 /*Why did return not work?
-Console log returns undefined which is annoying.*/
+Console log returns undefined which is annoying.
+^^ Because my returns are already inside of console.logs*/
